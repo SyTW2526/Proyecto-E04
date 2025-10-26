5129cd9ee1bd0b4fcc4d16c7758a313912c1c3ff
Adrian Martin <alu0101547619@ull.edu.es>
2025-10-26 16:48:12 +0000

    Actualicación item ingredient

--- server/src/items/ingredient.ts
+++ server/src/items/ingredient.ts
@@ -2,9 +2,6 @@ import { model, Schema } from 'mongoose';
 
 export interface IngredientInterface {
   name: string;
-  quantity: string;
-  unit?: string;
-  brand?: string;
 }
 
 const IngredientSchema = new Schema<IngredientInterface>({
@@ -13,19 +10,6 @@ const IngredientSchema = new Schema<IngredientInterface>({
     required: true,
     trim: true,
   },
-  quantity: { // Cantidad del ingrediente
-    type: String,
-    required: true,
-    trim: true,
-  },
-  unit: { // Unidad de medida como gramos, cucharadas, etc.
-    type: String,
-    trim: true,
-  },
-  brand: { 
-    type: String,
-    trim: true,
-  }
 }, { _id: false });
 
 export const Ingredient = model<IngredientInterface>('Ingredient', IngredientSchema); 
