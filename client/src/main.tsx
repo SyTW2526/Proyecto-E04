0e501ea5a0002cd7d79b7ed6e882fc27e3508010
alu0101580302 <alu0101580302@ull.edu.es>
2025-11-07 13:07:37 +0000

    Diseño de la página de receta

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import LogIn from './login.tsx'
+import Receipt from './receipt.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LogIn />
+    <Receipt />
   </StrictMode>,
 )
---
+++ client/src/receipt.tsx
@@ -0,0 +1,86 @@
+import { Home, Search, User, LogOut } from "lucide-react";
+import './receipt.css';
+
+function Receipt() {
+    return (
+        <>
+            <div className="ContenedorGeneralReceta">
+                <div className="NombreReceta">
+                    <h2>Pastel de pistacho</h2>
+                </div>
+                <div className="ContenedorReceta">
+                    <div className="InformacionReceta">
+                        <div className="IzquierdaReceta">
+                            <div className="UsuarioReceta">
+                                <img src="https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_640.png"></img>
+                                <p>Usuario</p>
+                            </div>
+                            <div className="ImagenesReceta">
+                                <img src="https://comedera.com/wp-content/uploads/sites/9/2023/03/pastel-de-pistache.jpeg" className="d-block w-100" alt="..."/>
+                            </div>
+                            <div className="CategoriasReceta">
+                                <h3>Categorías</h3>
+                                <div>Postre</div>
+                                <div>Dulce</div>
+                            </div>
+                            <div className="IngredientesReceta">
+                                <h3>Ingredientes</h3>
+                                <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+                            </div>
+                        </div>
+                        <div className="DerechaReceta">
+                            <div className="TextoReceta">
+                                <h3>Receta</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eros augue, mattis eu hendrerit ullamcorper, vulputate vel risus. Donec elementum erat eget enim tempor varius et id lorem. Aliquam luctus ornare dui id hendrerit. Etiam luctus iaculis nisi. Curabitur elementum, justo sed vestibulum dapibus, dolor enim aliquet diam, sed consequat sem turpis vel nibh. Vestibulum in leo a neque luctus molestie ut sed diam. Etiam nec volutpat elit. Sed vulputate auctor velit, ut volutpat dolor bibendum ac. Ut id metus sed nibh accumsan egestas. Duis posuere accumsan lectus, vitae sollicitudin massa feugiat vitae. Duis tincidunt, libero id congue pulvinar, lacus sapien consequat est, vitae laoreet dolor turpis vel lorem. Vestibulum eu diam ipsum. Maecenas pulvinar diam dignissim vehicula semper. Morbi auctor auctor justo, quis rutrum magna vulputate a. Cras dictum velit lacus, quis posuere augue lobortis vitae. Morbi vitae est sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eros augue, mattis eu hendrerit ullamcorper, vulputate vel risus. Donec elementum erat eget enim tempor varius et id lorem. Aliquam luctus ornare dui id hendrerit. Etiam luctus iaculis nisi. Curabitur elementum, justo sed vestibulum dapibus, dolor enim aliquet diam, sed consequat sem turpis vel nibh. Vestibulum in leo a neque luctus molestie ut sed diam. Etiam nec volutpat elit. Sed vulputate auctor velit, ut volutpat dolor bibendum ac. Ut id metus sed nibh accumsan egestas. Duis posuere accumsan lectus, vitae sollicitudin massa feugiat vitae. Duis tincidunt, libero id congue pulvinar, lacus sapien consequat est, vitae laoreet dolor turpis vel lorem. Vestibulum eu diam.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="ReseñasReceta">
+                        <h3>Reseñas</h3>
+                    </div>
+                </div>
+            </div>
+            <div className="ContenedorDerecha">
+                <div className="ContenedorPerfilSimp">
+                    <img className="LogoImagen" src="/logo.png" alt="Logo de ReceiptVault"></img>
+                    <img className="PerfilImagen" src="https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_640.png"></img>
+                    <p>Usuario</p>
+                    <div className="InformacionUsuario">
+                        <div>
+                            <p>300</p>
+                            <p>Seguidos</p>
+                        </div>
+                        <div>
+                            <p>300</p>
+                            <p>Seguidores</p>
+                        </div>
+                        <div>
+                            <p>300</p>
+                            <p>Posts</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="ContenedorNavegacion">
+                    <button className="BotonNavegacion">
+                        <Home size={24}></Home>
+                        <span>Inicio</span>
+                    </button>
+                    <button className="BotonNavegacion">
+                        <Search size={24}></Search>
+                        <span>Buscar</span>
+                    </button>
+                    <button className="BotonNavegacion">
+                        <User size={24}></User>
+                        <span>Perfil</span>
+                    </button>
+                    <button className="BotonNavegacion">
+                        <LogOut size={24}></LogOut>
+                        <span>Cerrar Sesión</span>
+                    </button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Receipt;
